ae557145a9576064a4ad62b6bf8a029f6f3905d9
estrella <epopo2@illinois.edu>
2023-01-31 02:10:53 -0600

    Add new page

--- src/lib/components/ProjectCard.svelte
+++ src/lib/components/ProjectCard.svelte
@@ -7,15 +7,15 @@
 </script>
 
     <div class="flex flex-col">
-        <a href={project_link}><img alt="project-card" src={project_image_link} class="rounded-md border-2 border-orange-400 hover:border-red-400" />
-        <h1 class="font-Unbounded mt-2 text-xl text-cyan-600 hover:text-orange-400">
+        <a href={project_link}><img alt="project-card" src={project_image_link} class="w-80 rounded-md border-2 border-orange-400 hover:border-red-400" />
+        <h1 class="font-Unbounded mt-2 text-xl text-cyan-600 hover:text-orange-400 text-center sm:text-left">
             {project_name}
         </h1>
         </a>
-        <p class="font-Sora text-gray-600">
+        <p class="font-Sora text-gray-600 text-center sm:text-left">
             {project_description}
         </p>
-        <p class="font-Sora text-gray-400 mt-2">
+        <p class="font-Sora text-gray-400 mt-2 text-center sm:text-left">
             {project_date}
         </p>
     </div>
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,17 +1,17 @@
 <script>
     import ProjectCard from '$lib/components/ProjectCard.svelte'
 </script>
-<div class="flex flex-col mx-14 md:mx-20 lg:mx-50">
-    <h2 class="font-Shrikhand text-red-300 text-4xl md:text-5xl mb-10">
-        works in progress <br>
-        and projects!✧
+<div class="flex flex-col mx-14 md:mx-20 xl:mx-60 items-center">
+    <h2 class="text-center font-Shrikhand text-red-300 text-4xl md:text-5xl mb-10">
+        ✧ projects!✧
     </h2>
 
-    <div class="flex flex-col md:grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 md:justify-center">
+    <div class="flex flex-col sm:grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10 md:justify-center">
         <ProjectCard
         project_name="Leyden Consulting Website Redesign"
-        project_image_link="/assets/lca.gif"
-        project_description="As a design consultant for IDC, I helped redesign our main website! Take a look at what we have built!"
+        project_image_link="/assets/LCA.gif"
+        project_description="I worked with Peggy Leyden, Founder of Leyden Consulting Associates, to redesign her website! Learn about the process and 
+        everything I changed from top to bottom."
         project_date="Dec 2022"
         project_link="/design/lca"
          />
@@ -30,12 +30,14 @@
             project_date="Sep 2022"
             project_link="/design/idc"
              />
-
         <ProjectCard
-              project_name="Illinois Design Consulting Website Redesign!"
-              project_image_link="/assets/idc.png"
-              project_description="As a design consultant for IDC, I helped redesign our main website! Take a look at what we have built!"
-                />
+            project_name="IS 445 Steam Games: Data Visualization in R "
+            project_image_link="/assets/IS445.png"
+            project_description="What are some of the best genres of Steam games? I used R to visualize the data from the past 10 years. Learn more
+            about the process and what I found!"
+            project_date="Dec 2022"
+            project_link="/design/is445"
+             />
         </div>
 </div>
 
